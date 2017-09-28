f658c0001502b6c3ec67196d3491e9bf15e2257c
Edgar De La Cruz <edelacruz1212@gmail.com>
2017-09-28 12:01:50 -0500

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { FormsModule } from '@angular/forms'; 
 
 import 'hammerjs';
 
@@ -19,6 +20,7 @@ import { ContactComponent } from './contact/contact.component';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -29,14 +31,19 @@ import { LeaderService } from './services/leader.service';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
 	BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-	AppRoutingModule
+	AppRoutingModule,
+	FormsModule
+  ],
+  entryComponents: [
+        LoginComponent
   ],
   providers: [DishService,
 			  PromotionService,
