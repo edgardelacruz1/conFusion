1a2e78ca187e6174be56c82fdb473eda0b5ade60
Edgar De La Cruz <edelacruz1212@gmail.com>
2017-09-29 10:02:22 -0500

    RxJs Part 1, Observable-Subscribe

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -3,7 +3,6 @@ import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 import { Observable } from 'rxjs/Observable';
 
-import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/observable/of';
 
@@ -12,11 +11,11 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Promise<Leader[]> {
-	  return Observable.of(LEADERS).delay(2000).toPromise();
+  getLeaders(): Observable<Leader[]> {
+	  return Observable.of(LEADERS).delay(2000);
   }
 
-  getFeaturedLeader(): Promise<Leader> {
-    return Observable.of(LEADERS.filter((leader) => leader.featured)[0]).delay(2000).toPromise();
+  getFeaturedLeader(): Observable<Leader> {
+    return Observable.of(LEADERS.filter((leader) => leader.featured)[0]).delay(2000);
   }
 }
